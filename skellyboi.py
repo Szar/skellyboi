b6d4f3855abf0672f83aea8518724d1290dee345
Szar <aly.hennessy@gmail.com>
2018-09-13 18:28:28 -0400

    Working with pyinstaller

--- skellyboi.py
+++ skellyboi.py
@@ -1,14 +1,16 @@
 #!"C:\Python27\python.exe"
+#import gevent
+import threading
+import dns
+from engineio import async_eventlet,async_threading #async_threading, async_gevent
 from threading import Lock
 from flask import Flask, render_template, session, request
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
 	close_room, rooms, disconnect
 import pyaudio, os, math, audioop, time
 from collections import deque
-from pyfiglet import Figlet
-
 """ THRESHOLD: Modify this to adjust minimum level to initiate animation """
-THRESHOLD = 15000
+THRESHOLD = 10000
 
 """ MIN_ANIMATION: Minimum seconds for animation to run """
 MIN_ANIMATION = 0.5
@@ -20,14 +22,20 @@ CHANNELS = 1
 RATE = 16000
 SILENCE_LIMIT = 1 
 RECORD_SECONDS = 1
-async_mode = None
+async_mode = 'threading'
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
-fig = Figlet(font='small')
-print(fig.renderText('SKELLYBOI'))
+
+print("""
+  ___ _       _ _      _         _ 
+ / __| |_____| | |_  _| |__  ___(_)
+ \__ \ / / -_) | | || | '_ \/ _ \ |
+ |___/_\_\___|_|_|\_, |_.__/\___/_|
+				  |__/    
+				  """)
 def background_thread():
 	p = pyaudio.PyAudio()
 	stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
@@ -36,14 +44,13 @@ def background_thread():
 	slid_win = deque(maxlen=SILENCE_LIMIT * rel)
 	avg_count = 4
 	ts = time.time()
-    
+	
 	while True:
 		cur_data = stream.read(CHUNK)
 		slid_win.append(math.sqrt(abs(audioop.avg(cur_data, avg_count))))
 		s = sum([round(x) for x in slid_win])
 		status = None
 		nstatus = 1 if s>THRESHOLD else 0
-		print(s)
 		if nstatus!=status and (nstatus==1 or time.time() - ts >= MIN_ANIMATION):
 			status=nstatus
 			ts = time.time()
@@ -68,4 +75,10 @@ def client_disconnect():
 	print('Client disconnected', request.sid)
 
 if __name__ == '__main__':
-	socketio.run(app, debug=False)
+	try:
+	 	socketio.run(app, debug=True)
+	except SystemExit as e:
+		print 'Error!', e
+		print 'Press enter to exit (and fix the problem)'
+		raw_input()
+	
